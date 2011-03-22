8754348a208ed309d41fea15e80b88809724d624
Chiradeep Vittal <chiradeep@cloud.com>
2011-03-22 14:16:14 -0700

    Global configurator

--- tools/test/cloudkit.py
+++ tools/test/cloudkit.py
@@ -1,9 +1,14 @@
 from apisession import ApiSession
 from physicalresource import ZoneCreator
+from globalconfig import GlobalConfig
 import random
     
 if __name__ == "__main__":
     api = ApiSession('http://localhost:8080/client/api', 'admin', 'password')
     api.login()
+    config = GlobalConfig(api)
+    config.update('use.local.storage', 'true')
+
     zonecreator = ZoneCreator(api, random.randint(2,1000))
     zonecreator.create()
+
