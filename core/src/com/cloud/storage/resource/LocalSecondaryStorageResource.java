b2c2b09a65e6f575bba7915766c8a4b179345e4a
Chiradeep Vittal <chiradeep@cloud.com>
2011-03-24 13:14:01 -0700

    move LocalSecondaryStorageResource to core

--- core/src/com/cloud/storage/resource/LocalSecondaryStorageResource.java
+++ core/src/com/cloud/storage/resource/LocalSecondaryStorageResource.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
-package com.cloud.storage.secondary;
+package com.cloud.storage.resource;
 
 import java.util.HashMap;
 import java.util.Map;
