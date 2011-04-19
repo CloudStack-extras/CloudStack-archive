7cb662b32bfd2338efe57f075de0e714fa09df23
will <will@cloud.com>
2011-04-18 20:09:44 -0700

    Adding registration token.

--- ui/cloudkit/scripts/cloudkit.hosts.js
+++ ui/cloudkit/scripts/cloudkit.hosts.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 		return;
 	}
 	$("#main").show();
-	$("#registration_complete_link").attr("href","https://my.rightscale.com/cloud_registrations/cloudkit/new?callback_url="+encodeURIComponent("http://localhost:8080/client/cloudkit/complete?token="+g_loginResponse.userid));
+	$("#registration_complete_link").attr("href","https://my.rightscale.com/cloud_registrations/cloudkit/new?callback_url="+encodeURIComponent("http://localhost:8080/client/cloudkit/complete?token="+g_loginResponse.registrationtoken));
 });
 
 
