02d0607f8c3c2a72cb7938d9449397e16ffe3eef
Sheng Yang <sheng.yang@cloud.com>
2011-05-31 16:59:46 -0700

    bug 9154: Initial check in for enabling redundant virtual router
    This patch enable redundant virtual routers.
    
    1. To enable this feature, db need to be updated using follow SQL by now(we
    would get a UI way later):
    
    UPDATE network_offerings SET redundant_router=1 WHERE guest_type="Virtual" AND
    system_only=0;
    
    2. System would try to start up two routers at different hosts. But if there is
    only one host in the zone, system would start up two routers on it.
    
    3. The failover part is using keepalived, and connection tracking part is using
    conntrackd. There would be one master router and one backup router. The status
    of router(master or backup) can be query from the database table domain_router
    now. Management server would update the status every 30s by default.
    
    4. The routers for the same zone would use same external NIC(same ip and mac).
    The script used for fail-over would ensure only one external NIC present in the
    network at any time.
    
    5. Currently management server don't got the ability to stop one of router is
    both of them reported as master. The feature is in the todo list.
    
    After two routers start up, disconnect anyone of them, the guest network
    shouldn't be affected, and established connection(http, ssh, etc.) should still
    works. The fail-over on gateway part should be 3~4 seconds.
    
    Currently the patch works with KVM. Would deal with vmware and XenServer soon.

--- server/src/com/cloud/api/ApiResponseHelper.java
+++ server/src/com/cloud/api/ApiResponseHelper.java
@@ -2376,6 +2376,7 @@ public class ApiResponseHelper implements ResponseGenerator {
         response.setSpecifyVlan(offering.getSpecifyVlan());
         response.setAvailability(offering.getAvailability().toString());
         response.setNetworkRate(ApiDBUtils.getNetworkRate(offering.getId()));
+        response.setRedundantRouter(offering.isRedundantRouterEnabled());
 
         if (offering.getGuestType() != null) {
             response.setGuestIpType(offering.getGuestType().toString());
