b1575863efdddcb583209659eb9fbe98201c5aba
alena <alena@cloud.com>
2011-09-02 12:03:21 -0700

    bug 11332: don't try to log vm's information when the vm is already Expunged status 11332: resolved fixed

--- server/src/com/cloud/vm/dao/VMInstanceDaoImpl.java
+++ server/src/com/cloud/vm/dao/VMInstanceDaoImpl.java
@@ -282,11 +282,17 @@ public class VMInstanceDaoImpl extends GenericDaoBase<VMInstanceVO, Long> implem
     	int result = update(vmi, sc);
     	if (result == 0 && s_logger.isDebugEnabled()) {
     		VMInstanceVO vo = findById(vm.getId());
-    		StringBuilder str = new StringBuilder("Unable to update ").append(vo.toString());
-    		str.append(": DB Data={Host=").append(vo.getHostId()).append("; State=").append(vo.getState().toString()).append("; updated=").append(vo.getUpdated()).append("; time=").append(vo.getUpdateTime());
-    		str.append("} New Data: {Host=").append(vm.getHostId()).append("; State=").append(vm.getState().toString()).append("; updated=").append(vmi.getUpdated()).append("; time=").append(vo.getUpdateTime());
-    		str.append("} Stale Data: {Host=").append(oldHostId).append("; State=").append(oldState).append("; updated=").append(oldUpdated).append("; time=").append(oldUpdateDate).append("}");
-    		s_logger.debug(str.toString());
+    		
+    		if (vo != null) {
+    		    StringBuilder str = new StringBuilder("Unable to update ").append(vo.toString());
+                str.append(": DB Data={Host=").append(vo.getHostId()).append("; State=").append(vo.getState().toString()).append("; updated=").append(vo.getUpdated()).append("; time=").append(vo.getUpdateTime());
+                str.append("} New Data: {Host=").append(vm.getHostId()).append("; State=").append(vm.getState().toString()).append("; updated=").append(vmi.getUpdated()).append("; time=").append(vo.getUpdateTime());
+                str.append("} Stale Data: {Host=").append(oldHostId).append("; State=").append(oldState).append("; updated=").append(oldUpdated).append("; time=").append(oldUpdateDate).append("}");
+                s_logger.debug(str.toString());
+    		} else {
+    		    s_logger.debug("Unable to update the vm id=" + vm.getId() + "; the vm either doesn't exist or already removed");
+    		}
+    		
     	}
     	return result > 0;
     }
