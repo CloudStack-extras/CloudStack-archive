fa2e5076f3fd3a85847e000f7054b54b72525bac
Jessica Wang <jessica@cloud.com>
2011-09-20 14:21:25 -0700

    cloudStack 3.0 new UI - configuration page - add System Service Offering.

--- ui-cloudstack3/js/configuration.js
+++ ui-cloudstack3/js/configuration.js
@@ -83,6 +83,83 @@
 		  }
         }
       },
+	  
+	  systemServiceOfferings: {
+        type: 'select',
+        title: 'System Service',
+        listView: {
+          label: 'System Service Offerings',
+          fields: {
+            name: { label: 'Name', editable: true },
+            storagetype: { label: 'Storage Type' },
+            cpuspeed: { label: 'CPU' },
+            memory: { label: 'Memory' },
+            domain: { label: 'Domain'}
+          },
+          actions: {
+            add: {
+              label: 'Add system service offering',
+
+              action: function(args) {
+                args.response.success();
+              },
+
+              messages: {
+                confirm: function(args) {
+                  return 'Are you sure you want to add ' + args.name + '?';
+                },
+                success: function(args) {
+                  return 'Your new system service offering is being created.';
+                },
+                notification: function(args) {
+                  return 'Created system service offering';
+                },
+                complete: function(args) {
+                  return 'Sysmte service offering has been created';
+                }
+              },
+
+              createForm: {
+                title: 'New system service offering',
+                desc: 'Please fill in the following data to add a new service offering.',
+                fields: {
+                  name: { label: 'Name', editable: true },
+                  displayText: { label: 'Display Text' },
+                  storageType: {
+                    label: 'Storage Type',
+                    select: [
+                      { id: 'shared', description: 'Shared' },
+                      { id: 'local', description: 'Local' }
+                    ]
+                  },
+                  cpuCores: { label: '# of CPU cores' },
+                  cpuSpeed: { label: 'CPU Speed (in MHz)'},
+                  memory: { label: 'Memory (in MB)' },
+                  tags: { label: 'Tags' },
+                  offerHA: { label: 'Offer HA', isBoolean: true },
+                  isPublic: { label: 'Public', isBoolean: true }
+                }
+              },
+
+              notification: {
+                poll: testData.notifications.testPoll
+              }
+            }
+          },
+          dataProvider: function(args) {          
+			$.ajax({
+			  url: createURL("listServiceOfferings&issystem=true&page="+args.page+"&pagesize="+pageSize),
+			  dataType: "json",
+			  async: true,
+			  success: function(json) { 	
+				var items = json.listserviceofferingsresponse.serviceoffering;			    
+				args.response.success({data:items});			                			
+			  }
+			});  	
+		  }
+        }
+      },
+	  
       diskOfferings: {
         type: 'select',
         title: 'Disk',
