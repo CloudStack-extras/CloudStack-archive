5cf80a9cc902d4f05d7d55cd7df19cd3f3a2f700
Brian Federle <brian@cloud.com>
2011-10-04 13:19:05 -0700

    Add missing description field

--- ui-cloudstack3/js-test/storage.js
+++ ui-cloudstack3/js-test/storage.js
@@ -153,6 +153,7 @@
                     label: 'OS Type',
                     select: function(args){
                       args.response.success({
+                        descriptionField: 'description',
                         data: [
                           { id: 'centos53-64', description: 'CentOS 5.3 (64-bit)' },
                           { id: 'rhel5-64', description: 'Red Hat Enterprise Linux 5.0 (64-bit)' },
