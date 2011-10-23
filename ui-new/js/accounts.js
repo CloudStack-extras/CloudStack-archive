82e09635d2a7574fd4b5fdd85be0934a3a89b2cf
Jessica Wang <jessica@cloud.com>
2011-10-23 14:54:35 -0700

    cloudStack 3.0 new UI - account page - add details tab.

--- ui-new/js/accounts.js
+++ ui-new/js/accounts.js
@@ -4,10 +4,7 @@
     id: 'accounts',
     listView: {
       fields: {
-        name: { 
-		  label: 'Name', 
-		  editable: true 
-	    },
+        name: { label: 'Name' },
 		accounttype: { 
 		  label: 'Role', 
 		  converter: function(args){
@@ -32,11 +29,84 @@
 		  async: true,
 		  success: function(json) { 	
 			var items = json.listaccountsresponse.account;			    
-			args.response.success({data:items});			               			
+			args.response.success({
+			  actionFilter: accountActionfilter,
+			  data:items
+			});			               			
 		  }
 		});  	
-	  }
+	  },
 	  
+	  //???
+	  detailView: {
+		name: 'Account details',   
+		
+		actions: {		
+		
+		},
+		
+		tabs: {
+		  details: {
+			title: 'details',
+						
+			fields: [
+			  {
+				name: { 
+				  label: 'Name', 
+				  isEditable: true 
+				}
+			  },
+			  {			    
+                id: { label: 'ID' },				
+				accounttype: { 
+				  label: 'Role', 
+				  converter: function(args){
+					return cloudStack.converters.toRole(args);          
+				  }
+				},
+				domain: { label: 'Domain' },
+				state: { label: 'State' },
+				vmtotal: { label: 'Total of VM' },
+				iptotal: { label: 'Total of IP Address' },
+				receivedbytes: { 
+				  label: 'Bytes received',
+                  converter: function(args) {					    
+					if (args == null || args == 0)
+					  return "";
+					else
+					  return cloudStack.converters.convertBytes(args);                       
+				  }						  
+				},
+				sentbytes: { 
+				  label: 'Bytes sent',
+				  converter: function(args) {					    
+					if (args == null || args == 0)
+					  return "";
+					else
+					  return cloudStack.converters.convertBytes(args);                       
+				  }		
+				}														
+			  }
+			],
+			
+			dataProvider: function(args) {	                  	
+			  args.response.success(
+				{
+				  actionFilter: accountActionfilter,
+				  data:args.context.accounts[0]
+				}
+			  );	
+			}			
+		  }		  
+		}
+	  }  
     }
-  };  
+  }; 
+
+  var accountActionfilter = function(args) {	    		  
+    var jsonObj = args.context.item;
+	var allowedActions = [];	
+	return allowedActions;
+  }
+  
 })(cloudStack, testData);
