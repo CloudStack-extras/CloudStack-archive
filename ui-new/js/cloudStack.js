55835a0986f2fd7c2080017bef6ea4390f0ce5e8
Brian Federle <brian@cloud.com>
2011-10-28 13:59:59 -0700

    Implement login

--- ui-new/js/cloudStack.js
+++ ui-new/js/cloudStack.js
@@ -21,19 +21,56 @@
     }
   });
 
-  login();
-
-  // Store capabilities
-  $.ajax({
-    url: createURL('listCapabilities'),
-    dataType: 'json',
-    async: false,
-    success: function(data) {
-      cloudStack._capabilities = data.listcapabilitiesresponse.capability;
-    }
-  });
-
   $(function() {
-    $('#cloudStack3-container').cloudStack(cloudStack);
+    var $container = $('#cloudStack3-container');
+    
+    cloudStack.uiCustom.login({
+      $container: $container,
+      
+      // Use this for checking the session, to bypass login screen
+      bypassLoginCheck: function(args) {
+        return false;
+        return {
+          user: {
+            login: 'wchan',
+            name: 'Will Chan'
+          }
+        };
+      },
+
+      // Actual login process, via form
+      loginAction: function(args) {
+        if (args.data.username != 'invalid') {
+          login();
+
+          // Store capabilities
+          $.ajax({
+            url: createURL('listCapabilities'),
+            dataType: 'json',
+            async: false,
+            success: function(data) {
+              cloudStack._capabilities = data.listcapabilitiesresponse.capability;
+            }
+          });
+          return args.response.success();
+        }
+
+        return args.response.error();
+      },
+
+      // Show cloudStack main UI widget
+      complete: function(args) {
+        $container.cloudStack($.extend(cloudStack, {
+          context: {
+            users: [
+              {
+                name: args.user.name,
+                login: args.user.login
+              }
+            ]
+          }
+        }));
+      }
+    });
   });
 })(jQuery, testData);
