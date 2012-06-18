130893be9389a30e3cc31a4a3191e7e1a176f42b
Vijayendra Bhamidipati <vijayendra.bhamidipati@citrix.com>
2012-06-18 14:59:40 -0700

    CS-15256: Unable to associate cluster with Nexus VSM upon a retry if addCluster command fails the first time
    Description:
    
    	Putting in code to clean up stale VSM records in case
    	addCluster fails to communicate with the vCenter server.

--- server/src/com/cloud/resource/ResourceManagerImpl.java
+++ server/src/com/cloud/resource/ResourceManagerImpl.java
@@ -580,6 +580,7 @@ public class ResourceManagerImpl implements ResourceManager, ResourceService, Ma
             if (!success) {
                 _clusterDetailsDao.deleteDetails(clusterId);
                 _clusterDao.remove(clusterId);
+                _clusterVSMDao.removeByClusterId(clusterId);
             }
         }
     }
