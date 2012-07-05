bc53d28836656a417da9cb8defd28c78597ef6ea
frank <frank.zhang@citrix.com>
2012-07-05 15:58:17 -0700

    support .htaccess in baremetal userdata

--- scripts/network/ping/baremetal_user_data.py
+++ scripts/network/ping/baremetal_user_data.py
@@ -10,11 +10,59 @@ import base64
 
 HTML_ROOT = "/var/www/html/"
 
+def writeIfNotHere(fileName, texts):
+    if not os.path.exists(fileName):
+    	entries = []
+    else:
+    	f = open(fileName, 'r')
+    	entries = f.readlines()
+    	f.close()
+
+    texts = [ "%s\n" % t for t in texts ]
+    need = False
+    for t in texts:
+        if not t in entries:
+            entries.append(t)
+            need = True
+            
+    if need: 
+        f = open(fileName, 'w')
+        f.write(''.join(entries))
+        f.close()
+    
+def createRedirectEntry(vmIp, folder, filename):
+    entry = "RewriteRule ^%s$  ../%s/%%{REMOTE_ADDR}/%s [L,NC,QSA]" % (fileName, folder, fileName)
+    htaccessFolder="/var/www/html/latest"
+    htaccessFile=os.path.join(htaccessFolder, ".htaccess")
+    if not os.path.exists(htaccessFolder):
+        os.makedirs(htaccessFolder)
+    writeIfNotHere(htaccessFile, ["Options +FollowSymLinks", "RewriteEngine On", entry])
+        
+    htaccessFolder = os.path.join("/var/www/html/", folder, vmIp)
+    if not os.path.exists(htaccessFolder):
+        os.makedirs(htaccessFolder)
+    htaccessFile=os.path.join(htaccessFolder, ".htaccess")
+    entry="Options -Indexes\nOrder Deny,Allow\nDeny from all\nAllow from %s" % vmIp
+    f = open(htaccessFile, 'w')
+    f.write(entry)
+    f.close()
+    
+    if folder in ['metadata', 'meta-data']:
+        entry1="RewriteRule ^meta-data/(.+)$  ../%s/%%{REMOTE_ADDR}/$1 [L,NC,QSA]" % folder
+        htaccessFolder="/var/www/html/latest"
+        htaccessFile=os.path.join(htaccessFolder, ".htaccess")
+        entry2="RewriteRule ^meta-data/$  ../%s/%%{REMOTE_ADDR}/meta-data [L,NC,QSA]" % folder
+        writeIfNotHere(htaccessFile, [entry1, entry2])
+        
+
 def addUserData(vmIp, folder, fileName, contents):
         
     baseFolder = os.path.join(HTML_ROOT, folder, vmIp)
     if not os.path.exists(baseFolder):
         os.makedirs(baseFolder)
+        
+    createRedirectEntry(vmIp, folder, fileName)
+    
     datafileName = os.path.join(HTML_ROOT, folder, vmIp, fileName)
     metaManifest = os.path.join(HTML_ROOT, folder, vmIp, "meta-data")
     if folder == "userdata":
@@ -28,16 +76,7 @@ def addUserData(vmIp, folder, fileName, contents):
     f.close()
     
     if folder == "metadata" or folder == "meta-data":
-        fm = open(metaManifest, 'a+')
-        metas = fm.readlines();
-        try:
-            metas.remove(fileName)
-        except ValueError, e:
-            pass
-        metas.append(fileName)
-        stuff = "\n".join(metas)
-        fm.write(stuff)
-        fm.close()
+	writeIfNotHere(metaManifest, fileName)
 
 if __name__ == '__main__':
     string = sys.argv[1]
