7dcf68fd1f04648b746f708eee33121ed13d03b3
Pranav Saxena <pranav.saxena@citrix.com>
2012-07-20 14:23:08 +0530

    AutoscaleUI development - adding the multi-edit tables hide effect

--- ui/scripts/ui-custom/autoscaler.js
+++ ui/scripts/ui-custom/autoscaler.js
@@ -16,19 +16,19 @@
     var bottomfields = args.forms.bottomFields;
     var scaleuppolicy = args.forms.scaleUpPolicy;
     var scaledownpolicy = args.forms.scaleDownPolicy;
-
+    
     return function(args) {
       var context = args.context;
       var $autoscalerDialog = $('<div>').addClass('autoscaler');
       var $topFields = $('<div>').addClass('field-group top-fields');
       var $bottomFields = $('<div>').addClass('field-group bottom-fields');
       var $scaleUpPolicy = $('<div>').addClass('scale-up-policy');
+      var $slideScaleUp = $('<div></div>').addClass('expand');
+      var $slideScaleDown = $('<div></div>').addClass('expand');
       var $scaleDownPolicy = $('<div>').addClass('scale-down-policy');
       var $scaleUpPolicyTitle = $('<div>').addClass('scale-up-policy-title').html("Scale Up Policy");
       var $scaleDownPolicyTitle = $('<div>').addClass('scale-down-policy-title').html("Scale Down Policy");
-      var topFieldForm, $topFieldForm,
-          bottomFieldForm, $bottomFieldForm;
-
+      var topFieldForm, $topFieldForm,bottomFieldForm, $bottomFieldForm;
       // Create and append top fields
       // -- uses create form to generate fields
       topFieldForm = cloudStack.dialog.createForm({
@@ -99,6 +99,7 @@
                 text: _l('Apply'),
                 'class': 'ok',
                 click: function() {
+                  cloudStack.autoscaler.actions.add({context: context});
                   $autoscalerDialog.dialog('close');
                   $('.overlay').remove();
                   $autoscalerDialog.closest(':ui-dialog').remove();
@@ -109,6 +110,14 @@
          $('.ui-dialog div.autoscaler div.form-container').find('.form-item[rel=templateNames] label').hide();
          $('div.ui-dialog div.autoscaler').find('div.scale-up-policy-title').append("<br></br>").append($inputLabel = $('<label>').html('Duration').attr({left:'200'})).append($('<input>').attr({ name: 'username' }));
          $('div.ui-dialog div.autoscaler').find('div.scale-down-policy-title').append("<br></br>").append($inputLabel = $('<label>').html('Duration').attr({left:'200'})).append($('<input>').attr({ name: 'username' }));
+         $('div.ui-dialog div.autoscaler div.scale-up-policy div.multi-edit div.data').prepend( $slideScaleUp);
+         $('div.ui-dialog div.autoscaler div.scale-down-policy div.multi-edit div.data').prepend( $slideScaleDown);
+
+         $('div.ui-dialog div.autoscaler div.scale-up-policy div.multi-edit div.data div.expand').click(function() { $('div.ui-dialog div.autoscaler div.scale-up-policy div.multi-edit div.data div.data-item').slideToggle(); });
+
+          $('div.ui-dialog div.autoscaler div.scale-down-policy div.multi-edit div.data div.expand').click(function() { $('div.ui-dialog div.autoscaler div.scale-down-policy div.multi-edit div.data div.data-item').slideToggle(); });
+
+       
       }
     }
 }(jQuery, cloudStack));
