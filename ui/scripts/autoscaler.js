d1c8909090a272988b66afe2f42d5fd4db0fca1d
Jessica Wang <jessica.wang@citrix.com>
2012-07-29 12:43:46 -0700

    cloudstack 3.0.x UI - autoscale - ceation - when login as regular-user, do not populate user dropdown by listUsers API since regular-user doesn't have access to listUsers API.

--- ui/scripts/autoscaler.js
+++ ui/scripts/autoscaler.js
@@ -305,21 +305,26 @@
           dependsOn: 'isAdvanced',
           label: 'User',
           select: function(args) {
-            $.ajax({
-              url: createURL('listUsers'),
-              data: {
-							  domainid: g_domainid,
-								account: g_account
-							},
-              success: function(json) {
-                var users = json.listusersresponse.user;
-                var items = [];                
-                $(users).each(function(){
-                  items.push({id: this.id, description: this.username});
-                });
-                args.response.success({ data:  items });
-              }
-            });
+					  var items = []; 
+						if(isAdmin() || isDomainAdmin()) {
+							$.ajax({
+								url: createURL('listUsers'),
+								data: {
+									domainid: g_domainid,
+									account: g_account
+								},
+								success: function(json) {
+									var users = json.listusersresponse.user;                               
+									$(users).each(function(){
+										items.push({id: this.id, description: this.username});
+									});
+									args.response.success({ data:  items });
+								}
+							});
+						}
+            else { //regular user doesn't have access to listUers API call.				
+							items.push({id: "", description: ""});
+						}						
           }
         }
       },
