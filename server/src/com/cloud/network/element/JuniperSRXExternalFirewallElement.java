4a773d9871b50b21e38dd68831d60d83aa196bb3
Sheng Yang <sheng.yang@citrix.com>
2012-09-05 16:28:11 -0700

    Elimiate mystery guestIp in IpAddressTO
    Nobody can explain why it's there...
    
    Also fix wrong return result of several empty network element callbacks

--- server/src/com/cloud/network/element/JuniperSRXExternalFirewallElement.java
+++ server/src/com/cloud/network/element/JuniperSRXExternalFirewallElement.java
@@ -546,7 +546,7 @@ PortForwardingServiceProvider, RemoteAccessVPNServiceProvider, IpDeployer, Junip
 
     @Override
     public boolean applyIps(Network network, List<? extends PublicIpAddress> ipAddress, Set<Service> service) throws ResourceUnavailableException {
-        // TODO Auto-generated method stub
-        return false;
+        // return true, as IP will be associated as part of static NAT/port forwarding rule configuration
+        return true;
     }
 }
