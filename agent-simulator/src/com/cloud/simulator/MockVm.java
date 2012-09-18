9b59a9e18a1ea1628653fa9627f003ec7072e205
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-09-18 23:19:10 +0530

    Using the priority to decide b/w MASTER/BACKUP
    BootArgs carry router priority for master and backup and simulator will
    reuse the priority to decide RvR status. Deprecating the odd/even logic.
    reviewed-by : Sangeetha Hariharan

--- agent-simulator/src/com/cloud/simulator/MockVm.java
+++ agent-simulator/src/com/cloud/simulator/MockVm.java
@@ -37,5 +37,8 @@ public interface MockVm {
 	public void setType(String type);
 	public void setVncPort(int vncPort);
 	public long getId();
+
+    public String getBootargs();
+    public void setBootargs(String bootargs);
 }
 
