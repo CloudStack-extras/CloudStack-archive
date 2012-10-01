262813c4996932831e3e4d6cc95299a186ff4ce6
Jessica Wang <jessica.wang@citrix.com>
2012-10-01 16:05:19 -0700

    CS-16197: cloudstack UI - Tags - input validation.

--- ui/scripts/ui/widgets/tagger.js
+++ ui/scripts/ui/widgets/tagger.js
@@ -15,11 +15,13 @@
     var value = $form.find('input[name=value]').val();
 
     if (!key || !value) {
-      cloudStack.dialog.notice({ message: 'Please specify a tag key and value' });
-      
+      cloudStack.dialog.notice({ message: 'Please specify a tag key and value' });      
       return false;
     }
-
+   
+    if($form.find('div.field.key').find('label.error').css('display') == 'block' || $form.find('div.field.value').find('label.error').css('display') == 'block')
+		  return false;
+		
     return true;
   };
   
@@ -28,10 +30,10 @@
       var $form = $('<form>').addClass('tag-input');
       var $keyField = $('<div>').addClass('field key');
       var $keyLabel = $('<label>').attr('for', 'key').html(_l('label.key') + ':');
-      var $key = $('<input>').addClass('key').attr('name', 'key');
+      var $key = $('<input>').addClass('key disallowSpecialCharacters').attr('name', 'key');
       var $valueField = $('<div>').addClass('field value');
       var $valueLabel = $('<label>').attr('for', 'value').html(_l('label.value') + ':');
-      var $value = $('<input>').addClass('value').attr('name', 'value');
+      var $value = $('<input>').addClass('value disallowSpecialCharacters').attr('name', 'value');
       var $submit = $('<input>').attr('type', 'submit').val('Add');
 
       $keyField.append($keyLabel, $key);
@@ -41,7 +43,7 @@
         $submit
       );
 
-      $form.validate({ onfocusout: false });
+      $form.validate();
 
       $form.submit(
         args.onSubmit ?
