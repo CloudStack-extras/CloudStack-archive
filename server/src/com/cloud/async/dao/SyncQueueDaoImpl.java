dd1f71dad8c0b2021486341d1fb0616da61b87ee
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2012-10-03 14:58:07 -0700

    Fixed lineendings and trailing whitespaces in a bunch of files

--- server/src/com/cloud/async/dao/SyncQueueDaoImpl.java
+++ server/src/com/cloud/async/dao/SyncQueueDaoImpl.java
@@ -34,12 +34,12 @@ public class SyncQueueDaoImpl extends GenericDaoBase<SyncQueueVO, Long> implemen
     private static final Logger s_logger = Logger.getLogger(SyncQueueDaoImpl.class.getName());
     
     SearchBuilder<SyncQueueVO> TypeIdSearch = createSearchBuilder();
-	
-	@Override
-	public void ensureQueue(String syncObjType, long syncObjId) {
-		Date dt = DateUtil.currentGMTTime();
-		String sql = "INSERT IGNORE INTO sync_queue(sync_objtype, sync_objid, created, last_updated) values(?, ?, ?, ?)";
-		
+    
+    @Override
+    public void ensureQueue(String syncObjType, long syncObjId) {
+        Date dt = DateUtil.currentGMTTime();
+        String sql = "INSERT IGNORE INTO sync_queue(sync_objtype, sync_objid, created, last_updated) values(?, ?, ?, ?)";
+        
         Transaction txn = Transaction.currentTxn();
         PreparedStatement pstmt = null;
         try {
@@ -50,24 +50,24 @@ public class SyncQueueDaoImpl extends GenericDaoBase<SyncQueueVO, Long> implemen
             pstmt.setString(4, DateUtil.getDateDisplayString(TimeZone.getTimeZone("GMT"), dt));
             pstmt.execute();
         } catch (SQLException e) {
-        	s_logger.warn("Unable to create sync queue " + syncObjType + "-" + syncObjId + ":" + e.getMessage(), e);
+            s_logger.warn("Unable to create sync queue " + syncObjType + "-" + syncObjId + ":" + e.getMessage(), e);
         } catch (Throwable e) {
-        	s_logger.warn("Unable to create sync queue " + syncObjType + "-" + syncObjId + ":" + e.getMessage(), e);
+            s_logger.warn("Unable to create sync queue " + syncObjType + "-" + syncObjId + ":" + e.getMessage(), e);
         }
-	}
-	
-	@Override
-	public SyncQueueVO find(String syncObjType, long syncObjId) {
-    	SearchCriteria<SyncQueueVO> sc = TypeIdSearch.create();
-    	sc.setParameters("syncObjType", syncObjType);
-    	sc.setParameters("syncObjId", syncObjId);
+    }
+    
+    @Override
+    public SyncQueueVO find(String syncObjType, long syncObjId) {
+        SearchCriteria<SyncQueueVO> sc = TypeIdSearch.create();
+        sc.setParameters("syncObjType", syncObjType);
+        sc.setParameters("syncObjId", syncObjId);
         return findOneBy(sc);
-	}
+    }
 
-	@Override @DB
-	public void resetQueueProcessing(long msid) {
-		String sql = "UPDATE sync_queue set queue_proc_msid=NULL, queue_proc_time=NULL where queue_proc_msid=?";
-		
+    @Override @DB
+    public void resetQueueProcessing(long msid) {
+        String sql = "UPDATE sync_queue set queue_proc_msid=NULL, queue_proc_time=NULL where queue_proc_msid=?";
+        
         Transaction txn = Transaction.currentTxn();
         PreparedStatement pstmt = null;
         try {
@@ -75,17 +75,17 @@ public class SyncQueueDaoImpl extends GenericDaoBase<SyncQueueVO, Long> implemen
             pstmt.setLong(1, msid);
             pstmt.execute();
         } catch (SQLException e) {
-        	s_logger.warn("Unable to reset sync queue for management server " + msid, e);
+            s_logger.warn("Unable to reset sync queue for management server " + msid, e);
         } catch (Throwable e) {
-        	s_logger.warn("Unable to reset sync queue for management server " + msid, e);
+            s_logger.warn("Unable to reset sync queue for management server " + msid, e);
         }
-	}
-	
-	protected SyncQueueDaoImpl() {
-	    super();
-	    TypeIdSearch = createSearchBuilder();
+    }
+    
+    protected SyncQueueDaoImpl() {
+        super();
+        TypeIdSearch = createSearchBuilder();
         TypeIdSearch.and("syncObjType", TypeIdSearch.entity().getSyncObjType(), SearchCriteria.Op.EQ);
         TypeIdSearch.and("syncObjId", TypeIdSearch.entity().getSyncObjId(), SearchCriteria.Op.EQ);
         TypeIdSearch.done();
-	}
+    }
 }
